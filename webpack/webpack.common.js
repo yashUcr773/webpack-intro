3b09191fa79ed6f4d41272d8a7814251a7493c6e
yashUcr773 <yagga004@ucr.edu>
2025-10-15 00:27:14 +0530

    2. different configs for prod and dev.

---
+++ webpack/webpack.common.js
@@ -0,0 +1,15 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'bundle.js',
+    },
+    mode: 'production',
+};
