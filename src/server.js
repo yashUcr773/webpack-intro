71a801aed79f35405adac3bbba7fe67743bbb2eb
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-10-19 14:43:56 +0530

    17.a serving static prod files using express

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.get('/', (req, res) => {
+    const absolutePathToHTML = path.resolve(__dirname, "../dist/main.html")
+    res.sendFile(absolutePathToHTML)
+})
+
+app.use('/static', express.static(path.resolve(__dirname, '../dist')))
+
+app.listen(3000, () => {
+    console.log('Running on PORT:3000, http://localhost:3000')
+})
--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -13,6 +13,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default merge(common, {
+    output: {
+        publicPath: '/static/',
+    },
     optimization: {
         minimize: true,
         minimizer: [
